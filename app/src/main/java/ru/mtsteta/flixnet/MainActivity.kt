426cdba38d9874718fecd9fc6ad7747f7fe8836c
alammer <coinup@mail.ru>
2021-07-20 15:54:41 +0300

    Add movies RV skeleton with fake data

--- app/src/main/java/ru/mtsteta/flixnet/MainActivity.kt
+++ app/src/main/java/ru/mtsteta/flixnet/MainActivity.kt
@@ -2,10 +2,21 @@ package ru.mtsteta.flixnet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import ru.mtsteta.flixnet.fakeRepo.MoviesDataSourceImpl
+import ru.mtsteta.flixnet.movies.MovieListAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_main_screen)
+
+        val movieRecycler = findViewById<RecyclerView>(R.id.rvMovieList)
+        val movies = MoviesDataSourceImpl().getMovies()
+        val adapter = MovieListAdapter(this)
+        movieRecycler.adapter = adapter
+        movieRecycler.layoutManager = GridLayoutManager(this, 2)
+        adapter.submitList(movies)
     }
 }
---
+++ app/src/main/java/ru/mtsteta/flixnet/movies/MovieListAdapter.kt
@@ -0,0 +1,62 @@
+package ru.mtsteta.flixnet.movies
+
+import android.content.Context
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import ru.mtsteta.flixnet.R
+
+class MovieListAdapter (context: Context, /*private val clickListener: MovieClickListener*/): ListAdapter<MovieDto, MovieListAdapter.MovieListViewHolder>(DiffCallback) {
+
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
+
+    class MovieListViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
+        //link item(object) elements with xml through databinding
+        private val posterImage: ImageView = itemView.findViewById<ImageView>(R.id.rvItemMoviePoster)
+        private val titleTextView: TextView = itemView.findViewById<TextView>(R.id.rvItemMovieTitle)
+        private val infoTextView: TextView = itemView.findViewById<TextView>(R.id.rvItemMovieInfo)
+        private val ageLimitTextView: TextView = itemView.findViewById<TextView>(R.id.rvItemAgeLimit)
+        private val ratingBar: RatingBar = itemView.findViewById<RatingBar>(R.id.rvItemMovieRating)
+
+        fun bind(/*clickListener: MovieClickListener, */item: MovieDto, position: Int) {
+            titleTextView.text = item.title
+            infoTextView.text = item.description
+            ratingBar.rating = item.rateScore.toFloat()
+            ageLimitTextView.text = item.ageRestriction.toString() + "+"
+            posterImage.load(item.imageUrl)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieListViewHolder {  //create new view by layout manager
+        val view = inflater.inflate(R.layout.movies_rv_item, parent, false)
+        return MovieListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MovieListViewHolder, position: Int) { //replace content of view by layout manager
+        holder.bind(/*clickListener, */getItem(position), position)
+    }
+
+
+    companion object DiffCallback : DiffUtil.ItemCallback<MovieDto>() {
+        override fun areItemsTheSame(oldItem: MovieDto, newItem: MovieDto): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: MovieDto, newItem: MovieDto): Boolean {
+            return oldItem.title == newItem.title
+        }
+
+    }
+}
+
+class MovieClickListener (val clickListener: (v: View, movie: MovieDto) -> Unit){
+    fun onClick(v: View, movie: MovieDto) = clickListener(v, movie)
+}
