fce1402835a4d21eb811270d56934ed80da37d35
alammer <coinup@mail.ru>
2021-07-27 21:09:23 +0300

    resolve merge conflict

--- app/src/main/java/ru/mtsteta/flixnet/MainActivity.kt
+++ app/src/main/java/ru/mtsteta/flixnet/MainActivity.kt
@@ -1,11 +1,50 @@
 package ru.mtsteta.flixnet
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import ru.mtsteta.flixnet.fakeRepo.MoviesDataSourceImpl
+import ru.mtsteta.flixnet.genres.GenreClickListener
+import ru.mtsteta.flixnet.genres.GenreListAdapter
+import ru.mtsteta.flixnet.movies.MovieClickListener
+import ru.mtsteta.flixnet.movies.MovieDto
+import ru.mtsteta.flixnet.movies.MovieListAdapter
+import ru.mtsteta.flixnet.movies.MovieSpaceItemDecoration
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_movie_details)
+        setContentView(R.layout.fragment_main_screen)
+
+        var fakeMovieData = MoviesDataSourceImpl()
+
+        val genreRecycler = findViewById<RecyclerView>(R.id.rvGenreList)
+        val genres = fakeMovieData.genreList
+        val genreAdapter = GenreListAdapter(genres, GenreClickListener { genre ->
+            showToast(genre)
+        })
+        genreAdapter.stateRestorationPolicy = RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
+        genreRecycler.adapter = genreAdapter
+
+        val movieRecycler = findViewById<RecyclerView>(R.id.rvMovieList)
+        val movies = fakeMovieData.getMovies()
+        val movieAdapter = MovieListAdapter(MovieClickListener { movieItem: MovieDto ->
+            showToast(movieItem.title)
+        })
+        movieAdapter.stateRestorationPolicy = RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
+        movieRecycler.adapter = movieAdapter
+        movieRecycler.addItemDecoration(MovieSpaceItemDecoration(resources.getDimensionPixelSize(R.dimen.mainscreen_movie_top_spacing)))
+        movieAdapter.submitList(movies)
+    }
+
+    private fun showToast(message: String?) {
+        when {
+            message.isNullOrEmpty() -> {
+                showToast(getString(R.string.movie_item_empty_message))
+            }
+            else -> Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        }
     }
 }
