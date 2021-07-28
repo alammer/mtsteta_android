dac5a9165abb8060e43d712fd558058777c19e2f
alammer <coinup@mail.ru>
2021-07-28 17:23:59 +0300

    customize bottom navigation view

--- app/src/main/java/ru/mtsteta/flixnet/MainActivity.kt
+++ app/src/main/java/ru/mtsteta/flixnet/MainActivity.kt
@@ -16,35 +16,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_main_screen)
-
-        var fakeMovieData = MoviesDataSourceImpl()
-
-        val genreRecycler = findViewById<RecyclerView>(R.id.rvGenreList)
-        val genres = fakeMovieData.genreList
-        val genreAdapter = GenreListAdapter(genres, GenreClickListener { genre ->
-            showToast(genre)
-        })
-        genreAdapter.stateRestorationPolicy = RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
-        genreRecycler.adapter = genreAdapter
-
-        val movieRecycler = findViewById<RecyclerView>(R.id.rvMovieList)
-        val movies = fakeMovieData.getMovies()
-        val movieAdapter = MovieListAdapter(MovieClickListener { movieItem: MovieDto ->
-            showToast(movieItem.title)
-        })
-        movieAdapter.stateRestorationPolicy = RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
-        movieRecycler.adapter = movieAdapter
-        movieRecycler.addItemDecoration(MovieSpaceItemDecoration(resources.getDimensionPixelSize(R.dimen.mainscreen_movie_top_spacing)))
-        movieAdapter.submitList(movies)
-    }
-
-    private fun showToast(message: String?) {
-        when {
-            message.isNullOrEmpty() -> {
-                showToast(getString(R.string.movie_item_empty_message))
-            }
-            else -> Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-        }
+        setContentView(R.layout.activity_main)
     }
 }
