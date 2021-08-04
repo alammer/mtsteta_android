d16d0c5d70e580f9921c10079c5915e66ddbd7ca
alammer <coinup@mail.ru>
2021-08-04 22:45:36 +0300

    Add mock network request with coroutines, add "pull to refresh" to main scree

--- app/src/main/java/ru/mtsteta/flixnet/MainActivity.kt
+++ app/src/main/java/ru/mtsteta/flixnet/MainActivity.kt
@@ -14,16 +14,23 @@ class MainActivity : AppCompatActivity() {
     private lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottomNavView)
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .add(R.id.main_container, MainScreenFragment())
                 .commit()
         }
-
-        bottomNavigationView = findViewById(R.id.bottomNavView)
+        else{
+            when (supportFragmentManager.findFragmentById(R.id.main_container)) {
+                is DetailFragment -> bottomNavigationView.menu.setGroupCheckable(0, false, true)
+            }
+        }
 
         supportFragmentManager.addOnBackStackChangedListener {
             when (supportFragmentManager.findFragmentById(R.id.main_container)) {
