3d430b44a886203d87eb1c736d7a043a9ae9512c
alammer <coinup@mail.ru>
2021-08-17 19:00:49 +0300

    add migration for DB versions

--- app/src/main/java/ru/mtsteta/flixnet/database/MovieDataBase.kt
+++ app/src/main/java/ru/mtsteta/flixnet/database/MovieDataBase.kt
@@ -3,10 +3,12 @@ package ru.mtsteta.flixnet.database
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import ru.mtsteta.flixnet.App
 
 
-@Database(entities = [Movie::class, Actor::class, Genre::class], version = 1)
+@Database(entities = [Movie::class, Actor::class, Genre::class], version = 2)
 abstract class MovieDataBase: RoomDatabase() {
 
     abstract val movieDataDao: MovieDataDao
@@ -14,7 +16,15 @@ abstract class MovieDataBase: RoomDatabase() {
     companion object {
         private const val DB_NAME = "Movies.db"
         val instance: MovieDataBase  by lazy {
-            Room.databaseBuilder(App.appContext, MovieDataBase::class.java, DB_NAME).build()
+            Room.databaseBuilder(App.appContext, MovieDataBase::class.java, DB_NAME)
+                .addMigrations(MIGRATION_1_2)
+                .build()
         }
+    }
+}
+
+internal val MIGRATION_1_2 = object : Migration(1, 2) {
+    override fun migrate(db: SupportSQLiteDatabase) {
+        db.execSQL("ALTER TABLE actor_table RENAME COLUMN photo TO poster")
     }
 }
